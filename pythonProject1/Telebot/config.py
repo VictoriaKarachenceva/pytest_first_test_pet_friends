d0beda38dbb1973938078eec04898745cd5544b8
ViktoriaKarachenceva <victoria.ben.99@gmail.com>
2022-11-26 22:19:33 +0300

    Итоговое задание 18.

---
+++ pythonProject1/Telebot/config.py
@@ -0,0 +1,10 @@
+TOKEN = '5895602379:AAGmDUPLm8tRKdd0zS8M0rbmxEP2B-Prpr0'
+
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
